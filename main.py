c97c5c6661a11295b692e632f45891990b844428
matinWFX <mtn.tvkli@gmail.com>
2026-07-26 19:45:15 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball((0, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -400:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
